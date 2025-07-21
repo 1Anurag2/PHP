ba3e8a3449d032e11c3cf87ce1e034f311011ac5
Anurag Verma <147230214+1Anurag2@users.noreply.github.com>
2025-07-21 09:33:19 +0530

    traits

--- Oops/traits.php
+++ Oops/traits.php
@@ -3,13 +3,24 @@ trait A
 {
     public function info()
     {
-        echo "This is an info method in trait A";
+        echo "This is an info method in trait A\n";
+    }
+    public function show()
+    {
+        echo "Hello from trait A\n";
     }
 }
 
+trait sayhello
+{
+    public function sayHello()
+    {
+        echo "Hello from trait sayhello\n";
+    }
+}
 class B
 {
-    use A;
+    use A, sayhello;
 }
 class C
 {
@@ -20,5 +31,8 @@ $c->info();
 echo "<br>";
 $b = new B();
 $b->info();
+$b->show();
+$b->sayHello();
+
 
 ?>
